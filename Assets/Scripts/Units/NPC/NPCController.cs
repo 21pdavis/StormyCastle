7c78e6784b59b6b58ee243d084a86c4847290ad0
Paul Davis <paulcdavis135@gmail.com>
2023-07-24 17:01:58 -0400

    Button prompt for interacting with the man + rudimentary controller/keyboard detection

--- Assets/Scripts/Units/NPC/NPCController.cs
+++ Assets/Scripts/Units/NPC/NPCController.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+using CallbackContext = UnityEngine.InputSystem.InputAction.CallbackContext;
+
 public abstract class NPCController : MonoBehaviour
 {
     public abstract void OnInteract();
--- Assets/Scripts/Units/NPC/Old Man/OldManController.cs
+++ Assets/Scripts/Units/NPC/Old Man/OldManController.cs
@@ -1,33 +1,62 @@
-using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+using CallbackContext = UnityEngine.InputSystem.InputAction.CallbackContext;
 
 public class OldManController : NPCController
 {
     [SerializeField] private OldManDialogue dialogue;
 
     private GameObject canvas;
+    private GameObject dialoguePart;
+    private GameObject uiPromptPart;
     private TextMeshProUGUI textMeshPro;
+
 
     private int currentLine = 0;
 
-    // TODO: bobbing animation on prompt
+    // TODO: bobbing animation for E prompt
     private void Start()
     {
         canvas = transform.Find("Canvas").gameObject;
-        textMeshPro = canvas.transform.Find("Dialogue").GetComponent<TextMeshProUGUI>();
+        dialoguePart = canvas.transform.Find("Dialogue Part").gameObject;
+        uiPromptPart = canvas.transform.Find("UI Prompt Part").gameObject;
+        textMeshPro = dialoguePart.transform.Find("Dialogue").GetComponent<TextMeshProUGUI>();
+    }
+
+    // TODO: more generic way of doing this
+    public void OnControlsChanged(CallbackContext context)
+    {
+        if (context.performed)
+        {
+            InputControl control = context.control;
+            if (control.parent.name.Contains("Gamepad") || (control.parent.parent != null && control.parent.parent.name.Contains("Gamepad")))
+            {
+                dialoguePart.transform.Find("Prompt").Find("PromptBox").Find("PromptText").GetComponent<TextMeshProUGUI>().text = "A";
+                uiPromptPart.transform.Find("Prompt").Find("PromptBox").Find("PromptText").GetComponent<TextMeshProUGUI>().text = "A";
+            }
+            else
+            {
+                dialoguePart.transform.Find("Prompt").Find("PromptBox").Find("PromptText").GetComponent<TextMeshProUGUI>().text = "E";
+                uiPromptPart.transform.Find("Prompt").Find("PromptBox").Find("PromptText").GetComponent<TextMeshProUGUI>().text = "E";
+            }
+        }
     }
 
     public override void OnInteract()
     {
-        if (!canvas.activeSelf)
+        if (!dialoguePart.activeSelf)
         {
-            canvas.SetActive(true);
+            uiPromptPart.SetActive(false);
+            dialoguePart.SetActive(true);
         }
 
         if (currentLine == dialogue.dialogueLines.Length)
         {
             currentLine = 0;
-            canvas.SetActive(false);
+            uiPromptPart.SetActive(true);
+            dialoguePart.SetActive(false);
             return;
         }
 
